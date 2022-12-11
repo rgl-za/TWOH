6e0e71d1789263abf9b0a69abe5e3de717f5b8e8
rgl-za <hyeonjeong2829@gmail.com>
2022-12-11 15:13:44 +0900

    [Fetch] 개발 환경 설정

---
+++ src/main/java/com/rglza/TWOH/TwohApplication.java
@@ -0,0 +1,13 @@
+package com.rglza.TWOH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwohApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rglza/TWOH/TwohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rglza.TWOH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
